4915fce093aa0ef183a1983cd3d7b69210929be7
Titus Popovici <popovici.titus@yahoo.com>
2015-08-02 21:42:57 +0300

    basic add/remove functionality, history of ops

--- public/app/app.js
+++ public/app/app.js
@@ -5,7 +5,37 @@
     var app = angular.module(appName, ['ngRoute']);
 
     app.controller('walletCtrl', function($scope){
+        var add = 'Addition';
+        var subtract = 'Subtraction';
 
+        $scope.operations = [];
+        $scope.budget = 50;
+        $scope.amount = "";
+
+        $scope.addAmount = function(){
+            $scope.budget += $scope.amount;
+            addToOperations(add);
+            resetInputs();
+        };
+
+        $scope.subtractAmount = function(){
+            $scope.budget -= $scope.amount;
+            addToOperations(subtract);
+            resetInputs();
+        };
+
+        function resetInputs(){
+            $scope.amount = "";
+        }
+
+        function addToOperations(type){
+            $scope.operations.push({
+                amount: $scope.amount,
+                remainingBudget: $scope.budget,
+                type: type,
+                date: new Date()
+            })
+        }
     })
 
     .config(function($routeProvider) {
